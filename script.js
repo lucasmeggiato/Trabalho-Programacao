95f14dddcf4cd857daa987e669039ed7230f1618
Lucas Estival Meggiato <lucasmeggiato@gmail.com>
2026-02-25 21:28:53 -0300

    att. final

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-const toggleBtn = document.getElementById('theme-toggle');
-const body = document.body;
-const saudacaoEl = document.getElementById('saudacao');
- 
 const horaAtual = new Date().getHours();
+let saudacao = '';
+
 if (horaAtual >= 5 && horaAtual < 12) {
-    saudacaoEl.textContent = '☕ Bom dia!';
+  saudacao = 'Bom dia!';
 } else if (horaAtual >= 12 && horaAtual < 18) {
-    saudacaoEl.textContent = '☀️ Boa tarde!';
+  saudacao = 'Boa tarde!';
 } else {
-    saudacaoEl.textContent = '🌙 Boa noite!';
+  saudacao = 'Boa noite!';
 }
-
-const temaSalvo = localStorage.getItem('temaPreferido');
-
-if (temaSalvo === 'escuro') {
-    body.classList.add('dark-mode');
-    toggleBtn.textContent = '☀️ Modo Claro';
-}
-
-toggleBtn.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    
-    if (body.classList.contains('dark-mode')) {
-        toggleBtn.textContent = '☀️ Modo Claro';
-        localStorage.setItem('temaPreferido', 'escuro'); // Salva no navegador
-    } else {
-        toggleBtn.textContent = '🌙 Modo Escuro';
-        localStorage.setItem('temaPreferido', 'claro'); // Salva no navegador
-    }
-});
